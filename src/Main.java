be351e28d075bcc77fdbe958e7738080669f6f49
Natalia Sheliah <sheliahnatalia@gmail.com>
2023-12-04 13:35:04 +0200

    Step 8

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,10 @@
 
 public class Main {
-    public static void main(String[] args) {
-try {
-    StringCalculator calc = new StringCalculator();
-//    System.out.println(calc.add(""));
-//    System.out.println(calc.add("4"));
-//    System.out.println(calc.add("4,7"));
-    //System.out.println(calc.add("//;\n1;-2"));
+    public static void main(String[] args) throws Exception {
 
-    System.out.println(calc.add("//[*a*][__]\n1*a*2__3"));
-}
-catch( Exception  e) {
-    System.out.println(e.getMessage());
-}
+        StringCalculator calc = new StringCalculator();
+
+        System.out.println(calc.add("//[*][!]\n1*2!2,8\n1"));
     }
+
 }
